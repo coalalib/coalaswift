fd6d10053c5ada2a3852801d12f409099312075f
Pavel Shatalov <shatalovp@gmail.com>
2018-03-21 15:53:55 +0700

    Change ProxyLayer options

--- Source/ProxyLayer.swift
+++ Source/ProxyLayer.swift
@@ -38,20 +38,32 @@ extension ProxyLayer: InLayer {
 extension ProxyLayer: OutLayer {
 
     func run(coala: Coala, message: inout CoAPMessage, toAddress: inout Address) throws {
-        if let proxyAddress = message.proxyViaAddress {
-            message.setOption(.proxyUri, value: message.url?.absoluteString)
-            toAddress = proxyAddress
+        guard let proxyAddress = message.proxyViaAddress else { return }
+        guard let url = message.url,
+              let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
+              let scheme = components.scheme,
+              let host = components.host,
+              let port = components.port else {
+                message.setOption(.proxyUri, value: nil)
+                toAddress = proxyAddress
+                return
         }
+        let proxyUri = "\(scheme)://\(host):\(port)"
+        message.setOption(.proxyUri, value: proxyUri)
+        toAddress = proxyAddress
+
         // https://tools.ietf.org/html/rfc7252#section-5.10.2
         // The Proxy-Uri Option MUST take precedence over any of the Uri-Host,
         // Uri-Port, Uri-Path or Uri-Query options (each of which MUST NOT be
         // included in a request containing the Proxy-Uri Option).
-        if message.getOptions(.proxyUri).first != nil {
-            message.removeOption(.uriHost)
-            message.removeOption(.uriPort)
-            message.removeOption(.uriPath)
-            message.removeOption(.uriQuery)
-        }
+
+        // According latest news we shouldn't remove uri options from original message
+        // if message.getOptions(.proxyUri).first != nil {
+        //  message.removeOption(.uriHost)
+        //  message.removeOption(.uriPort)
+        //  message.removeOption(.uriPath)
+        //   message.removeOption(.uriQuery)
+        //}
     }
 
 }
